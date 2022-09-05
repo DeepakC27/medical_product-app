a2e48958c8b6e78428329e59fbc14018689abccf
Deepak Chaudhari <deepakchaudhari@Campk12-MacBook-Air.local>
2022-09-05 10:34:46 +0530

    routes init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PageLoader from 'components/PageLoader';
+import { APP_ROUTES } from 'config/enum';
+import React, { Suspense } from 'react';
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+  Navigate
+} from 'react-router-dom';
 
-function App() {
+const HomePage = React.lazy(() => import('./container/Homepage'))
+const ProductDetails = React.lazy(() => import('./container/ProductDetails'))
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <Suspense fallback={<PageLoader />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={APP_ROUTES.HOME} element={<HomePage />} />
+          <Route path={APP_ROUTES.SEARCH} element={<div>Search</div>} />
+          <Route path={`${APP_ROUTES.PRODUCT}/:productId`} element={<ProductDetails />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  )
+};
+export default App
